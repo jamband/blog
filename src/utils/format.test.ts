a3e9ae8e06ba9a06ebe64239b27fdfb637565f4d
Tomoki Morita <tmsongbooks215@gmail.com>
2021-07-30 22:19:34 +0900

    Add describe in some tests

--- src/utils/format.test.ts
+++ src/utils/format.test.ts
@@ -1,9 +1,11 @@
 import { formatDate } from "./format";
 
-test("1999-12-31", () => {
-  expect(formatDate("1999-12-31")).toBe("Dec 31, 1999");
-});
+describe("formatDate", () => {
+  test("1999-12-31", () => {
+    expect(formatDate("1999-12-31")).toBe("Dec 31, 1999");
+  });
 
-test("2000-01-01", () => {
-  expect(formatDate("2000-01-01")).toBe("Jan 01, 2000");
+  test("2000-01-01", () => {
+    expect(formatDate("2000-01-01")).toBe("Jan 01, 2000");
+  });
 });
--- src/utils/meta.test.ts
+++ src/utils/meta.test.ts
@@ -1,12 +1,14 @@
 import { APP_DESCRIPTION } from "~/constants/app";
 import { description } from "./meta";
 
-test("", () => {
-  const content = "<h1>Foo<h1><p>description...</p>";
-  expect(description(content)).toBe("description...");
-});
+describe("description", () => {
+  test("", () => {
+    const content = "<h1>Foo<h1><p>description...</p>";
+    expect(description(content)).toBe("description...");
+  });
 
-test("don't have a description section", () => {
-  const content = "<h1>Foo</h1>";
-  expect(description(content)).toBe(APP_DESCRIPTION);
+  test("don't have a description section", () => {
+    const content = "<h1>Foo</h1>";
+    expect(description(content)).toBe(APP_DESCRIPTION);
+  });
 });
