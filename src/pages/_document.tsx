917cc367e8347054e3a71921d99d5ed2fc9a079d
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-31 21:18:53 +0900

    Use functional component in pages/_document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import { APP_NAME } from "~/constants/app";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="ja">
-        <Head>
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content={APP_NAME} />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={APP_NAME} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
