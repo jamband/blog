58479a7b46c6531b4da974a36f23804706727658
Tomoki Morita <tmsongbooks215@gmail.com>
2021-12-24 21:51:21 +0900

    Improve some styles on tags page

--- src/pages/tags/[tag].tsx
+++ src/pages/tags/[tag].tsx
@@ -46,22 +46,25 @@ export default function View(props: Props) {
 
   return (
     <Page title={tag}>
-      <h2>Tags</h2>
       <Head>
         <meta name="description" content={APP_DESCRIPTION} />
         <meta property="og:title" content={`${tag} ･ ${APP_NAME}`} />
         <meta property="og:description" content={APP_DESCRIPTION} />
         <meta property="og:url" content={`${APP_URL}tags/${tag}`} />
       </Head>
-      <Tags tags={props.tags} className="mb-8" decoration />
-      <h2>
-        Posts <span className="text-xs text-gray-400 tracking-widest">#</span>
-        <span className="text-sm text-pink-500">{tag}</span>
+      <h2 className="mb-5">Tags</h2>
+      <Tags tags={props.tags} className="mb-14" decoration />
+      <h2 className="mb-4">
+        Posts{" "}
+        <span className="font-normal text-xs text-gray-400 tracking-widest">
+          #
+        </span>
+        <span className="font-normal text-base text-pink-500">{tag}</span>
       </h2>
       <ul>
         {props.posts.map((post) => (
           <li key={post.slug} className="mb-6">
-            <div className="text-xs italic text-gray-400">
+            <div className="italic text-xs text-gray-400">
               {formatDate(post.date)}
             </div>
             <Link href={`/${post.year}/${post.month}/${post.slug}`}>
