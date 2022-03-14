4b339e9ab4d124b09d75ee1b40f456317901fbb4
Tomoki Morita <tmsongbooks215@gmail.com>
2022-03-15 02:45:09 +0900

    Minor style change

--- src/components/home-link/component.tsx
+++ src/components/home-link/component.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Component: React.VFC = () => (
   <Link href="/">
-    <a className="rounded bg-gray-700 px-5 pt-2 pb-3 text-gray-200 no-underline active:text-pink-500">
+    <a className="rounded bg-gray-700 px-5 py-3 text-gray-200 no-underline active:text-pink-500">
       ← Home
     </a>
   </Link>
