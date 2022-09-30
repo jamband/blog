0c55c2ac51b6339e788b6d8981f3800a8bcaca06
Tomoki Morita <tmsongbooks215@gmail.com>
2022-10-01 01:09:14 +0900

    remove some unnecessary styles

--- src/components/home-link/component.tsx
+++ src/components/home-link/component.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Component: React.FC = () => (
   <Link href="/">
-    <a className="inline-flex rounded bg-gray-700 px-4 py-1 text-sm text-gray-400 no-underline hover:text-gray-100 active:text-gray-100 active:ring-2 active:ring-gray-500">
+    <a className="rounded bg-gray-700 px-4 py-1 text-sm text-gray-400 no-underline hover:text-gray-100 active:text-gray-100 active:ring-2 active:ring-gray-500">
       ← home
     </a>
   </Link>
