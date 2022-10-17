85795345570bf7147985099384f7549798430eae
Tomoki Morita <tmsongbooks215@gmail.com>
2022-10-17 20:22:23 +0900

    little more vertical space for tags

--- src/components/tags/component.tsx
+++ src/components/tags/component.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import type { _Props } from "./types";
 
 export const Component: React.FC<_Props> = (props) => (
-  <ul className={`flex flex-wrap gap-4 ${props.className || ""}`}>
+  <ul className={`flex flex-wrap gap-x-4 gap-y-5 ${props.className || ""}`}>
     {props.tags.map((tag) => (
       <li key={tag}>
         <Link href={`/tags/${tag}`}>
