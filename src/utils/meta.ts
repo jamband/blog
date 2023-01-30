edb1fc5fda8f079f4835636505c9be16afadaa8d
Tomoki Morita <tmsongbooks215@gmail.com>
2023-01-31 00:32:57 +0900

    append trim in description function

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -2,5 +2,6 @@ export const description = (content: string) => {
   return content
     .slice(0, 180)
     .replace(/#.*|\n|\(.*\)|\[|\]/g, "")
-    .slice(0, 90);
+    .slice(0, 90)
+    .trim();
 };
