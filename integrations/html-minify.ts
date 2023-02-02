1b6e10fcdc0de318217d2a30ca595c3c769d39de
Tomoki Morita <tmsongbooks215@gmail.com>
2023-02-02 20:54:19 +0900

    close #3: html-minify is only applied to specific pages

--- integrations/html-minify.ts
+++ integrations/html-minify.ts
@@ -3,40 +3,37 @@ import { minify } from "html-minifier-terser";
 import { readFile, writeFile } from "node:fs/promises";
 
 export default function htmlMinify(): AstroIntegration {
-  const name = "html-minify";
-
   return {
-    name,
+    name: "html-minify",
     hooks: {
-      "astro:build:done": async ({ routes }) => {
-        await Promise.all(
-          routes.map(async ({ distURL }) => {
-            if (distURL && !distURL.pathname.includes("rss.xml")) {
-              const path = distURL.pathname;
+      "astro:build:done": async ({ dir, pages }) => {
+        pages.map(async (page) => {
+          const path =
+            page.pathname === "404/"
+              ? `${dir.pathname}${page.pathname.slice(0, -1)}.html`
+              : `${dir.pathname}${page.pathname}index.html`;
 
-              const data = await minify(await readFile(path, "utf8"), {
-                collapseBooleanAttributes: true,
-                collapseWhitespace: true,
-                conservativeCollapse: true,
-                decodeEntities: true,
-                ignoreCustomComments: [/^#/],
-                minifyCSS: true,
-                minifyJS: true,
-                removeAttributeQuotes: true,
-                removeComments: true,
-                removeOptionalTags: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                sortAttributes: true,
-                sortClassName: true,
-              });
+          const data = await minify(await readFile(path, "utf8"), {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            decodeEntities: true,
+            ignoreCustomComments: [/^#/],
+            minifyCSS: true,
+            minifyJS: true,
+            removeAttributeQuotes: true,
+            removeComments: true,
+            removeOptionalTags: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            sortAttributes: true,
+            sortClassName: true,
+          });
 
-              await writeFile(path, data, "utf8");
-            }
-          })
-        );
+          await writeFile(path, data, "utf8");
+        });
 
-        console.log(`\x1b[32m${name}:\x1b[0m completed.`);
+        console.log(`\x1b[32mhtml-minify:\x1b[0m completed.`);
       },
     },
   };
