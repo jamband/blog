254f2cd0effcaeef232708c0699098638f65aaf3
Tomoki Morita <tmsongbooks215@gmail.com>
2023-02-04 16:17:53 +0900

    remove unnecessary some metacharacter [skip ci]

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -2,7 +2,7 @@ export const description = (content: string) => {
   return content
     .slice(0, 180)
     .replace(/#.*|\n/g, "")
-    .replace(/\[(.*)?\]\(.*\)/g, "$1")
+    .replace(/\[(.*)\]\(.*\)/g, "$1")
     .slice(0, 90)
     .trim();
 };
