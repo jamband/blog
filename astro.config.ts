230c7590ab85ae30fd3ccb587948bfd4186645fb
Tomoki Morita <tmsongbooks215@gmail.com>
2023-05-22 18:45:01 +0900

    use "class" option for scopedStyleStrategy

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   site: "https://jamband.github.io/",
   base: "/blog",
   trailingSlash: "always",
+  scopedStyleStrategy: "class",
   integrations: [htmlMinify(), sitemap()],
   markdown: {
     rehypePlugins: [rehypeExternalLink, rehypePrettyCode],
