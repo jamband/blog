a8c52bdc31ce3447203ed324db82d184cec2b241
Tomoki Morita <tmsongbooks215@gmail.com>
2023-11-29 18:24:40 +0900

    provide global apis for vitest

--- src/utils/format.test.ts
+++ src/utils/format.test.ts
@@ -1,4 +1,3 @@
-import { describe, expect, test } from "vitest";
 import { dateFormat } from "./format";
 
 describe("dateFormat", () => {
--- src/utils/meta.test.ts
+++ src/utils/meta.test.ts
@@ -1,4 +1,3 @@
-import { describe, expect, test } from "vitest";
 import { description } from "./meta";
 
 describe("description", () => {
