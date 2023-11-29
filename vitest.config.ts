6267989a2d8b2c5f6c91b58888835ec1f8e2d985
Tomoki Morita <tmsongbooks215@gmail.com>
2023-11-29 18:19:29 +0900

    fix vitest settings
    when i upgraded the astro version to 3.6, a type error occurred, so i changed it as a temporary solution.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
-/// <reference types="vitest" />
-import { getViteConfig } from "astro/config";
+import { defineConfig } from "vitest/config";
 
-export default getViteConfig({
+export default defineConfig({
   test: {
     globals: true,
     include: ["**/*.test.ts"],
